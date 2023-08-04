92d6ccdcf584afb3d190eeebdd028367bc183569
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-08-05 00:46:32 +0100

    added about section for desktops

--- components/homepage/AboutSection.tsx
+++ components/homepage/AboutSection.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 
 const AboutSection = () => {
   return (
-    <section className="min-h-screen bg-[#0cc0df] px-[1.5rem] relative pb-[5rem]">
+    <section className="min-h-screen bg-[#0cc0df] px-[1.5rem] md:px-[4rem] relative pb-[5rem] md:pb-[8rem] md:flex md:justify-center md:items-center md:gap-[2rem] lg:gap-[4rem] md:pt-[5rem]">
       <Image
         src={flower}
         width={100}
         height={100}
         alt="flower"
-        className="absolute right-[1.5rem] top-[2.5rem] animate-spin-slow"
+        className="absolute right-[1.5rem] top-[2.5rem] md:top-[5.8rem] animate-spin-slow z-[10]"
       />
-      <div className="pt-[11rem]">
+      <div className="pt-[11rem] md:pt-[5rem] md:w-[50%]">
         <h1 className="text-[1.5rem] font-semibold tracking-wider] font-louize">
           ABOUT
         </h1>
@@ -22,7 +22,7 @@ const AboutSection = () => {
           Loved Youngsters' Vision for{" "}
           <span className="italic">Courageous Faith</span>
         </h1>
-        <p className=" mt-[2rem] mb-[3rem]">
+        <p className="mt-[2rem] md:text-[0.8rem] lg:text-[0.95rem] leading-[2rem] mb-[3rem]">
           Loved youngsters is an organization aimed at raising spirit-filled
           children and teenagers who walk boldly in the will and power of God
           through profound teachings of the gospel and faith based products.{" "}
@@ -37,18 +37,18 @@ const AboutSection = () => {
 
         <Link
           href={"/"}
-          className="border-[1px] hover:bg-[#94edfc] duration-300 border-black uppercase text-[0.8rem] tracking-widest px-[0.8rem] py-[0.7rem]"
+          className="border-[1px] hover:bg-[#94edfc] duration-300 border-black uppercase text-[0.8rem] tracking-widest px-[0.8rem] py-[0.7rem] hover:font-semibold"
         >
           Learn More
         </Link>
       </div>
-      <div className="mt-[4rem] rounded-lg border-2">
+      <div className="mt-[4rem] rounded-lg border-2 w-fit h-fit mx-auto overflow-hidden group cursor-pointer">
         <Image
           src={aboutimg}
           alt="about"
           width={500}
           height={200}
-          className="rounded-lg"
+          className="rounded-lg md:group-hover:scale-[1.05] duration-300"
         />
       </div>
     </section>

321ef115c1e027ebd903b80de20e168e99bcf3b5
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-08-09 15:10:02 +0100

    added animations to about page

--- components/aboutPage/Values.tsx
+++ components/aboutPage/Values.tsx
@@ -2,78 +2,101 @@ import Image from "next/image";
 import mission from "@/assets/mission.svg";
 import flower from "@/assets/flower.svg";
 import creed from "@/assets/creed.svg";
+import { Reveal } from "../utils/Reveal";
+import { ImageReveal } from "../utils/ImageReveal";
 
 const Values = () => {
   return (
     <section className="min-h-screen px-[1.5rem] lg:px-[5rem] bg-[#35e1ff] pb-[5rem] pt-[2rem]">
-      <h1 className="text-[2.5rem] font-louize-medium text-center py-[3rem] lg:mb-[1rem]">
-        the SOUL of LOVED YOUNGSTERS
-      </h1>
+      <Reveal y={80} width="100%">
+        <h1 className="text-[2.5rem] font-louize-medium text-center py-[3rem] lg:mb-[1rem]">
+          the SOUL of LOVED YOUNGSTERS
+        </h1>
+      </Reveal>
       <div className="flex flex-col lg:flex-row gap-[4rem] justify-center items-center lg:items-start text-center">
         <div className="lg:w-[70%]">
-          <Image
-            src={mission}
-            alt="mission"
-            width={50}
-            className="mx-auto mb-[1.5rem] animate-spin-slow"
-          />
+          <ImageReveal>
+            <Image
+              src={mission}
+              alt="mission"
+              width={50}
+              className="mx-auto mb-[1.5rem] animate-spin-slow"
+            />
+          </ImageReveal>
           <div>
-            <p className="uppercase tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
-              Mission Statement
-            </p>
-            <p className="italic lg:text-[0.9rem]">
-              <span className="font-semibold">
-                TO RAISE TEENAGERS WHO ARE :
-              </span>{" "}
-              <br />
-              ~ Immersed fully into the love of Christ
-              <br />
-              ~ Indoctrinated into the teachings of the Christian Faith
-              <br /> ~ Understand Intimacy with God
-            </p>
+            <Reveal y={80} width="100%">
+              <p className="uppercase tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
+                Mission Statement
+              </p>
+            </Reveal>
+            <Reveal y={150} width="100%">
+              <p className="italic lg:text-[0.9rem]">
+                <span className="font-semibold">
+                  TO RAISE TEENAGERS WHO ARE :
+                </span>{" "}
+                <br />
+                ~ Immersed fully into the love of Christ
+                <br />
+                ~ Indoctrinated into the teachings of the Christian Faith
+                <br /> ~ Understand Intimacy with God
+              </p>
+            </Reveal>
           </div>
         </div>
         <div className="lg:w-[70%] lg:translate-x-[1rem]">
-          <Image
-            src={creed}
-            alt="creed"
-            width={50}
-            className="mx-auto mb-[1.5rem] animate-spin-slow"
-          />
+          <ImageReveal>
+            <Image
+              src={creed}
+              alt="creed"
+              width={50}
+              className="mx-auto mb-[1.5rem] animate-spin-slow"
+            />
+          </ImageReveal>
           <div>
-            <p className="uppercase lg:text-[0.9rem] tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
-              Our Creed
-            </p>
-            <p className="italic lg:text-[0.9rem]">
-              Abba's Love found us. <br />
-              Now He is our treasure. <br />
-              Abba rejoices over us with Joy. <br />
-              Now we delight in Him. <br />
-              Abba's light is shining in our hearts, <br />
-              Now we light up the world. <br />
-              Abba's spirit is burning in our bones, <br />
-              Now we set the world ablaze for our King. <br />
-              We are young and loved. <br />
-              We are loved youngsters. <br />
-            </p>
+            <Reveal y={80} width="100%">
+              <p className="uppercase lg:text-[0.9rem] tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
+                Our Creed
+              </p>
+            </Reveal>
+            <Reveal y={150} width="100%">
+              <p className="italic lg:text-[0.9rem]">
+                Abba's Love found us. <br />
+                Now He is our treasure. <br />
+                Abba rejoices over us with Joy. <br />
+                Now we delight in Him. <br />
+                Abba's light is shining in our hearts, <br />
+                Now we light up the world. <br />
+                Abba's spirit is burning in our bones, <br />
+                Now we set the world ablaze for our King. <br />
+                We are young and loved. <br />
+                We are loved youngsters. <br />
+              </p>
+            </Reveal>
           </div>
         </div>
         <div className="lg:w-[70%]">
-          <Image
-            src={flower}
-            alt="flower"
-            width={50}
-            className="mx-auto mb-[1.5rem] animate-spin-slow"
-          />
+          <ImageReveal>
+            <Image
+              src={flower}
+              alt="flower"
+              width={50}
+              className="mx-auto mb-[1.5rem] animate-spin-slow"
+            />
+          </ImageReveal>
           <div>
-            <p className="uppercase tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
-              Core Values
-            </p>
-            <p className="italic lg:text-[0.9rem]">
-              ~IMMERSION <br />
-              ~INDOCTRINATION <br />
-              ~INTIMACY <br />
-            </p>
+            <Reveal y={80} width="100%">
+              <p className="uppercase tracking-widest underline mb-[1rem] font-semibold font-louize-Bold">
+                Core Values
+              </p>
+            </Reveal>
+
+            <Reveal y={100} width="100%">
+              <p className="italic lg:text-[0.9rem]">
+                ~IMMERSION <br />
+                ~INDOCTRINATION <br />
+                ~INTIMACY <br />
+              </p>
+            </Reveal>
           </div>
         </div>
       </div>
--- components/aboutPage/Vision.tsx
+++ components/aboutPage/Vision.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import visionsvg from "@/assets/vision.svg";
+import { Reveal } from "../utils/Reveal";
+import { ImageReveal } from "../utils/ImageReveal";
 
 const Vision = () => {
   return (
@@ -10,12 +12,17 @@ const Vision = () => {
         alt="vision"
         className="absolute top-[4rem] md:top-[2rem] lg:top-[4rem] animate-spin-slow"
       />
+
       <div className="flex flex-col gap-[1.5rem] font-semibold tracking-wider">
-        <p className="text-center uppercase underline">Vision Statement</p>
-        <h1 className="uppercase text-[#0cc0df] font-louize-Bold text-center text-[2rem] lg:text-[3.5rem] font-bold tracking-wide md:w-[70%] mx-auto strokeblack italic">
-          ~We Envision all teenagers believing and delighting in the love of
-          Christ~
-        </h1>
+        <Reveal y={80} width="100%">
+          <p className="text-center uppercase underline">Vision Statement</p>
+        </Reveal>
+        <Reveal y={150} width="100%">
+          <h1 className="uppercase text-[#0cc0df] font-louize-Bold text-center text-[2rem] lg:text-[3.5rem] font-bold tracking-wide md:w-[70%] mx-auto strokeblack italic">
+            ~We Envision all teenagers believing and delighting in the love of
+            Christ~
+          </h1>
+        </Reveal>
       </div>
     </section>
   );

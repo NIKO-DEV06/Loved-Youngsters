6469a9110e2f723b2e9eb94c4bca54a4e7dcd9f4
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-08-14 18:54:29 +0100

    added image and confidentianlity message

--- components/contactPage/ContactPage.tsx
+++ components/contactPage/ContactPage.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import emailjs from "@emailjs/browser";
 import toast, { Toaster } from "react-hot-toast";
+import contact from "@/assets/contact.png";
+import { ImageReveal } from "../utils/ImageReveal";
 
 const ContactPage = () => {
   const [isSending, setIsSending] = useState(false);
@@ -54,136 +57,148 @@ const ContactPage = () => {
   };
 
   return (
-    <section className=" min-h-screen py-[8rem] md:pt-[3rem] bg-[#35e1ff]">
-      <Toaster position="top-center" reverseOrder={false} />
-      <form
-        id="contact-form"
-        onSubmit={handleSubmit(submitFormHandler)}
-        className="pt-[1.5rem] flex flex-col items-center justify-start"
-      >
-        <div className="flex flex-col gap-[1rem] lg:gap-[1.5rem]">
-          <div className="flex flex-col items-center md:items-start">
-            <motion.h1
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-              className="text-[1.7rem] italic font-louize-Bold lg:text-[2.3rem] lg:w-[25rem] text-center md:text-left md:mx-0 mx-auto w-[20rem] font-semibold md:font-[500] mb-[1rem]"
-            >
-              Love to hear from you, Get in touch{" "}
-              <motion.span
-                animate={{ rotate: [0, 10, -8, 0] }}
-                transition={{
-                  duration: 0.8,
-                  repeat: Infinity,
-                  repeatType: "reverse",
-                  ease: "linear",
-                }}
-                className="animate-pulse inline-block"
+    <>
+      <section className=" min-h-screen py-[8rem] md:pt-[3rem] bg-[#35e1ff]">
+        <Toaster position="top-center" reverseOrder={false} />
+        <ImageReveal>
+          <Image
+            src={contact}
+            alt="contact-us"
+            width={500}
+            className="mx-auto px-[2rem] rounded-lg"
+          />
+        </ImageReveal>
+        <form
+          id="contact-form"
+          onSubmit={handleSubmit(submitFormHandler)}
+          className="pt-[1.5rem] flex flex-col items-center justify-start"
+        >
+          <div className="flex flex-col gap-[1rem] lg:gap-[1.5rem]">
+            <div className="flex flex-col items-center md:items-start">
+              <motion.h1
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                className="text-[1.7rem] italic font-louize-Bold lg:text-[2.3rem] lg:w-[25rem] text-center md:text-left md:mx-0 mx-auto w-[20rem] font-semibold md:font-[500] mb-[1rem]"
               >
-                💐
-              </motion.span>
-            </motion.h1>
-          </div>
+                Love to hear from you, Get in touch{" "}
+                <motion.span
+                  animate={{ rotate: [0, 10, -8, 0] }}
+                  transition={{
+                    duration: 0.8,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    ease: "linear",
+                  }}
+                  className="animate-pulse inline-block"
+                >
+                  💐
+                </motion.span>
+              </motion.h1>
+            </div>
 
-          <div className="flex flex-col md:flex-row gap-[1rem] items-start">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-              className="flex flex-col gap-[0.4rem]"
-            >
-              <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
-                Your Name
-              </p>
-              <input
-                placeholder="e.g. John Doe"
-                type="text"
-                {...register("name")}
-                name="name"
-                autoComplete="off"
-                className={`inputs ${
-                  errors.name?.message
-                    ? "border-[#ff0000] focus:border-[#ff0000]"
-                    : "border-black focus:border-black"
-                }`}
-              />
-              <p className="text-[#ff0000] font-[500] text-sm text-left pt-1">
-                {errors.name?.message}
-              </p>
-            </motion.div>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-              className="flex flex-col gap-[0.4rem]"
-            >
-              <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
-                Your Number
-              </p>
-              <input
-                {...register("number")}
-                placeholder="e.g. +2347012345678"
-                type="number"
-                name="number"
-                className={`inputs ${
-                  errors.number?.message
-                    ? "border-[#ff0000] focus:border-[#ff0000]"
-                    : "border-black focus:border-black"
-                }`}
-              />
-              <p className="text-[#ff0000] font-[500] text-sm text-left pt-1">
-                {errors.number?.message}
-              </p>
-            </motion.div>
-          </div>
+            <div className="flex flex-col md:flex-row gap-[1rem] items-start">
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                className="flex flex-col gap-[0.4rem]"
+              >
+                <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
+                  Your Name
+                </p>
+                <input
+                  placeholder="e.g. John Doe"
+                  type="text"
+                  {...register("name")}
+                  name="name"
+                  autoComplete="off"
+                  className={`inputs ${
+                    errors.name?.message
+                      ? "border-[#ff0000] focus:border-[#ff0000]"
+                      : "border-black focus:border-black"
+                  }`}
+                />
+                <p className="text-[#ff0000] font-[500] text-sm text-left pt-1">
+                  {errors.name?.message}
+                </p>
+              </motion.div>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                className="flex flex-col gap-[0.4rem]"
+              >
+                <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
+                  Your Number
+                </p>
+                <input
+                  {...register("number")}
+                  placeholder="e.g. +2347012345678"
+                  type="number"
+                  name="number"
+                  className={`inputs ${
+                    errors.number?.message
+                      ? "border-[#ff0000] focus:border-[#ff0000]"
+                      : "border-black focus:border-black"
+                  }`}
+                />
+                <p className="text-[#ff0000] font-[500] text-sm text-left pt-1">
+                  {errors.number?.message}
+                </p>
+              </motion.div>
+            </div>
 
-          <div className="flex flex-col items-center md:items-start">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-              className="flex flex-col gap-[0.4rem]"
-            >
-              <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
-                Message
-              </p>
-              <textarea
-                {...register("message")}
-                name="message"
-                cols={30}
-                rows={10}
-                autoComplete="off"
-                placeholder="Type your message here.."
-                className={`outline-none ${
-                  errors.message?.message
-                    ? "border-[#ff0000] focus:border-[#ff0000]"
-                    : "border-black focus:border-black"
-                } border-[1.8px] bg-transparent placeholder:text-black h-[15rem] w-[21rem] md:w-[43rem] lg:w-[55rem] rounded-none appearance-none resize-none px-4 py-3 text-[0.95rem] lg:text-[1rem]`}
-              />
-              <p className="text-[#ff0000] font-[500] text-sm text-center">
-                {errors.message?.message}
-              </p>
-            </motion.div>
+            <div className="flex flex-col items-center md:items-start">
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                className="flex flex-col gap-[0.4rem]"
+              >
+                <p className="tracking-wider lg:text-[1.2rem] font-louize-Bold font-bold">
+                  Message
+                </p>
+                <textarea
+                  {...register("message")}
+                  name="message"
+                  cols={30}
+                  rows={10}
+                  autoComplete="off"
+                  placeholder="Type your message here.."
+                  className={`outline-none ${
+                    errors.message?.message
+                      ? "border-[#ff0000] focus:border-[#ff0000]"
+                      : "border-black focus:border-black"
+                  } border-[1.8px] bg-transparent placeholder:text-black h-[15rem] w-[21rem] md:w-[43rem] lg:w-[55rem] rounded-none appearance-none resize-none px-4 py-3 text-[0.95rem] lg:text-[1rem]`}
+                />
+                <p className="text-[#ff0000] font-[500] text-sm text-center">
+                  {errors.message?.message}
+                </p>
+              </motion.div>
+            </div>
+            <div className="flex flex-col items-center md:items-start">
+              <motion.button
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ duration: 0 }}
+                variants={variants}
+                disabled={isSending}
+                className={`group flex justify-center items-center gap-[0.5rem] bg-black text-white w-[21rem] lg:w-[27rem] p-[0.65rem] mt-[1rem] tracking-widest md:hover:bg-[#94edfc] md:hover:text-black duration-200 border-[1.8px] border-black ${
+                  isSending ? "opacity-50" : ""
+                }`}
+              >
+                {isSending ? "SENDING..." : "SEND"}
+              </motion.button>
+            </div>
           </div>
-          <div className="flex flex-col items-center md:items-start">
-            <motion.button
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              whileTap={{ scale: 0.9 }}
-              transition={{ duration: 0 }}
-              variants={variants}
-              disabled={isSending}
-              className={`group flex justify-center items-center gap-[0.5rem] bg-black text-white w-[21rem] lg:w-[27rem] p-[0.65rem] mt-[1rem] tracking-widest md:hover:bg-[#94edfc] md:hover:text-black duration-200 border-[1.8px] border-black ${
-                isSending ? "opacity-50" : ""
-              }`}
-            >
-              {isSending ? "SENDING..." : "SEND"}
-            </motion.button>
-          </div>
-        </div>
-        {/* <p>* Your information is safe with us. Confidentiality guaranteed.</p> */}
-      </form>
-    </section>
+        </form>
+        <p className="text-center text-[0.9rem] px-[2rem] mt-[2rem] opacity-60">
+          Your information is safe with us. Confidentiality guaranteed
+        </p>
+      </section>
+    </>
   );
 };
 
